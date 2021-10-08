b0bac476a2e600d471f0a8c620b245569b8e29a6
vorabkin <vorabkin@gmail.com>
2021-10-08 14:31:04 +0300

    final commit for v1,v2, v3

--- src/Hello.java
+++ src/Hello.java
@@ -3,11 +3,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
-<<<<<<< HEAD
-		System.out.println("HW Hello v1, v2");
-=======
-		System.out.println("HW Hello v3");
->>>>>>> refs/heads/hw-v3
+		System.out.println("HW Hello v1, v2, v3");
 
 	}
 
